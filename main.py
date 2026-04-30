a1bda2231bb5d529532bf7f8d5bc8759d15a6308
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2026-04-30 13:44:21 -0400

    Script now work with parameters

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from python-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("Missing api_key value")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=f"{args.user_prompt}"
+    )
+    if response.usage_metadata == None:
+        raise RuntimeError("Fail to communicate with Gemini API")
+
+    print(f"Prompt tokens: {response.usage_metadata .prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
 
 
 if __name__ == "__main__":
