682e9eb10d604c9ce3268dfee3dc314c4707aa23
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2026-04-30 21:59:23 -0400

    Project in starnge state

---
+++ functions/get_files_info.py
@@ -0,0 +1,28 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    result = ""
+    try:
+        abs_working_directory = os.path.abspath(working_directory)
+        abs_directory = os.path.normpath(os.path.join(abs_working_directory, directory))
+
+        valid_abs_directory = os.path.commonpath([abs_working_directory, abs_directory]) == abs_working_directory
+        if not valid_abs_directory:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        existing_directory = os.path.exists(abs_directory)
+        if not existing_directory:
+            return f'Error: "{directory}" is not a directory'
+
+        directory_content = os.listdir(abs_directory)
+        more_content_info = []
+        for content in directory_content:
+            full_path_content = os.path.join(abs_directory, content)
+            more_content_info.append(f" - {content}: file_size={os.path.getsize(full_path_content)}, is_dir={os.path.isdir(full_path_content)}")
+
+        result = '\n'.join(more_content_info)
+    except Exception as e:
+        result = f"Error: {e}"
+
+    return f"Result for current directory:\n{result}"
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -13,19 +14,25 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     client = genai.Client(api_key=api_key)
 
+    from google.genai import types
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     response = client.models.generate_content(
         model='gemini-2.5-flash',
-        contents=f"{args.user_prompt}"
+        contents=messages
     )
     if response.usage_metadata == None:
         raise RuntimeError("Fail to communicate with Gemini API")
-
-    print(f"Prompt tokens: {response.usage_metadata .prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if (args.verbose):
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
